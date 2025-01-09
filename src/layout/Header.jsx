8f9a2d8137d4fa4792985e933ded4bcc7049f484
himnandinisharma <himnandinisharma@gmail.com>
2025-01-09 19:52:18 +0530

    Task : Removed public prefix from image src Components : Header.jsx,categories.json,products.json

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -23,14 +23,14 @@ function Header() {
   return (
     <>
     <div className="flex justify-between px-8 py-4 items-center border-b-black border-b">
-        <div className="logo w-36"><img src="/public/logo/logo.webp" alt="" /></div>
+        <div className="logo w-36"><img src="/logo/logo.webp" alt="" /></div>
         <div className="menu flex">
             <h1 className='hover:bg-blue-100 py-2 px-4 rounded-lg cursor-pointer' onClick={openHome}>Home</h1>
             <h1 className='hover:bg-blue-100 py-2 px-4 rounded-lg cursor-pointer'onClick={openShop}>Shop</h1>
             <h1 className='hover:bg-blue-100 py-2 px-4 rounded-lg cursor-pointer'onClick={openAbout}>About</h1>
             <h1 className='hover:bg-blue-100 py-2 px-4 rounded-lg cursor-pointer'onClick={openContact}>Contact</h1>
         </div>
-        <div className="cart w-6"onClick={openCart}><img src="/public/Icons/ic_cart.svg" alt="" /></div>
+        <div className="cart w-6"onClick={openCart}><img src="/Icons/ic_cart.svg" alt="" /></div>
 
     </div>
     </>
